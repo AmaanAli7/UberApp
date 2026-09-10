c1d158402a5ec0588a13e565e8ec4fca102ce7e5
amaan ali <amaanalikhaan58833@gmail.com>
2026-09-10 10:25:06 +0530

    Yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import  { useContext } from 'react';
 import {Route,Routes} from 'react-router-dom';
-import Start from './pages/start';
+import Start from './pages/Start';
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
